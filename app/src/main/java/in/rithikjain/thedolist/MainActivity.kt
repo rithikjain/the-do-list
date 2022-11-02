28a39eea2fb4916e5ce84ede885e449a6164f9df
Rithik Jain <rithik.jain3006@gmail.com>
2022-11-03 01:24:14 +0530

    feat: update main activity

--- app/src/main/java/in/rithikjain/thedolist/MainActivity.kt
+++ app/src/main/java/in/rithikjain/thedolist/MainActivity.kt
@@ -1,30 +1,26 @@
 package `in`.rithikjain.thedolist
 
-import `in`.rithikjain.thedolist.utils.Constants
-import `in`.rithikjain.thedolist.widget.TheDoListWidget
-import `in`.rithikjain.thedolist.widget.TheDoListWidgetReceiver
-import androidx.datastore.preferences.core.mutablePreferencesOf
-import androidx.glance.appwidget.ExperimentalGlanceRemoteViewsApi
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import com.google.android.glance.tools.viewer.GlanceSnapshot
-import com.google.android.glance.tools.viewer.GlanceViewerActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-@OptIn(ExperimentalGlanceRemoteViewsApi::class)
-class MainActivity : GlanceViewerActivity() {
+class MainActivity : ComponentActivity() {
 
-    override suspend fun getGlanceSnapshot(
-        receiver: Class<out GlanceAppWidgetReceiver>,
-    ): GlanceSnapshot {
-        return when (receiver) {
-            TheDoListWidgetReceiver::class.java -> GlanceSnapshot(
-                instance = TheDoListWidget(),
-                state = mutablePreferencesOf(
-                    Constants.PREF_SELECTED_TAB_ID to 1,
-                )
-            )
-            else -> throw IllegalArgumentException()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize(),
+                color = Color(0xFF161616)
+            ) {
+
+            }
         }
     }
-
-    override fun getProviders() = listOf(TheDoListWidgetReceiver::class.java)
 }
